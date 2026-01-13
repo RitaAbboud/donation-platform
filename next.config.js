570fe3d3cb5918ccd123fc5c960bf73fe9d092ba
Jana Al Abed <janaalabed0101@gmail.com>
2026-01-13 11:35:04 +0200

    adjusting the quality for the image!

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
-};
-
+  images: {
+    // Add 100 to the allowed qualities array
+    qualities: [75, 100],
+  },
+}
 export default nextConfig;
